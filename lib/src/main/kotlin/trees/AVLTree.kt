137c15ba009cce0abea47157426bdd823ba654a6
Sofya Kozyreva <k81sofia@gmail.com>
2024-03-23 23:52:04 +0300

    feat(AVL): implement balancing methods for the tree

--- lib/src/main/kotlin/trees/AVLTree.kt
+++ lib/src/main/kotlin/trees/AVLTree.kt
@@ -30,11 +30,38 @@ class AVLTree<K : Comparable<K>, V>: balancedTree<K, V, AVLNode<K, V>>() {
         return getHeight(node.rightChild) - getHeight(node.leftChild)
     }
 
+    /* balances a tree by performing left & right rotations
+    if absolute value of balance factor is more than 1 */
+    private fun balance (curNode: AVLNode<K, V>) {
+        when (getBalanceFactor(curNode)) {
+            -2 -> {
+                curNode.leftChild?.let { if (getBalanceFactor(it) == 1) rotateLeft(it, findParent(it)) }
+                rotateRight(curNode, findParent(curNode))
+            }
+            2 -> {
+                curNode.rightChild?.let { if (getBalanceFactor(it) == -1) rotateRight(it, findParent(it)) }
+                rotateLeft(curNode, findParent(curNode))
+            }
+            else -> updateHeight(curNode)
+        }
+        findParent(curNode)?.let { balanceAfterInsert(it) }
+    }
+
     override fun balanceAfterInsert(curNode: AVLNode<K, V>) {
-        TODO("Not yet implemented")
+        balance(curNode)
     }
 
     override fun balanceAfterDelete(curNode: AVLNode<K, V>) {
-        TODO("Not yet implemented")
+        balance(curNode)
+    }
+
+    override fun rotateRight (node: AVLNode<K, V>, parentNode:  AVLNode<K, V>?) {
+        super.rotateRight(node, parentNode)
+        updateHeight(node)
+    }
+
+    override fun rotateLeft (node: AVLNode<K, V>, parentNode:  AVLNode<K, V>?) {
+        super.rotateLeft(node, parentNode)
+        updateHeight(node)
     }
 }
