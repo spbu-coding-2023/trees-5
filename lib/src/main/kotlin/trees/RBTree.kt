e2b7ab57e878d989dba6f57117c6c3f32ef5dd2d
Shvorob <soniashv23@gmail.com>
2024-03-29 01:52:44 +0300

    feat(RBT): implement method of insertion

--- lib/src/main/kotlin/trees/RBTree.kt
+++ lib/src/main/kotlin/trees/RBTree.kt
@@ -1,4 +1,42 @@
-package trees
+import nodes.RBNode
+import trees.balancedTree
 
-class RBTree {
+
+class RBTree<K : Comparable<K>, V>: balancedTree<K, V, RBNode<K, V>>() {
+    private fun getGrandparent(node: RBNode<K, V>): RBNode<K, V>? {
+        val parent = findParent(node) ?: return null
+        val grandparent = findParent(parent)
+        return grandparent
+    }
+
+    private fun getSibling(node: RBNode<K, V>): RBNode<K, V>? {
+        val parent = findParent(node) ?: return null
+        return if (node == parent.leftChild) {
+            parent.rightChild
+        } else {
+            parent.leftChild
+        }
+    }
+
+    private fun getUncle(node: RBNode<K, V>): RBNode<K, V>? {
+        val parent = findParent(node) ?: return null
+        val grandparent = findParent(parent)
+        return if (parent == grandparent?.leftChild) {
+            grandparent.rightChild
+        } else {
+            grandparent?.leftChild
+        }
+    }
+    override fun insert(key: K, value: V) {
+        val insertedNode = insertNode(key, value) ?: throw IllegalArgumentException("Nothing to insert")
+        balance(insertedNode, true)
+    }
+    override fun createNewNode(key: K, value: V): RBNode<K, V> {
+        TODO("Not yet implemented")
+    }
+
+    override fun balance(curNode: RBNode<K, V>, isAfterInsert: Boolean) {
+        TODO("Not yet implemented")
+    }
+
 }
