22fbc42ec716a21b60056524f577111c34e48860
Shvorob <soniashv23@gmail.com>
2024-03-30 15:51:17 +0300

    feat(RBT): Impement method balanceAfterInsert

--- lib/src/main/kotlin/trees/RBTree.kt
+++ lib/src/main/kotlin/trees/RBTree.kt
@@ -1,3 +1,4 @@
+import nodes.Color
 import nodes.RBNode
 import trees.balancedTree
 
@@ -40,7 +41,71 @@ class RBTree<K : Comparable<K>, V>: balancedTree<K, V, RBNode<K, V>>() {
         else { balanceAfterDelete(curNode) }
     }
     private fun balanceAfterInsert(curNode: RBNode<K, V>) {
-        TODO("Not yet implemented")
+        var insertedNode = curNode
+        insertedNode.color = Color.RED
+
+        /* case 1: insertedNode is root */
+        if (root == insertedNode) {
+            insertedNode.color = Color.BLACK
+            return
+        }
+
+        var parent = findParent(insertedNode) ?: throw IllegalArgumentException("Non-root should have parent")
+
+        /* case 2: parent of insertedNode is black */
+        if (parent.color == Color.BLACK) {
+            return
+        }
+
+        val uncle = getUncle(insertedNode)
+        val grandparent = getGrandparent(insertedNode) ?: throw IllegalArgumentException("Every node by that point should have grandparent")
+
+        /* case 3: uncle is non-null and red (so both uncle and parent are red) */
+        if ((uncle != null) && (uncle.color == Color.RED)) {
+            parent.color = Color.BLACK
+            uncle.color = Color.BLACK
+            grandparent.color = Color.RED
+            balanceAfterInsert(grandparent)
+            return
+        }
+
+        /* case 4: uncle is black; grandparent, parent and node form a "triangle"
+        *    G                        G
+        *   /                          \
+        *  P    - left triangle         P   - right triangle
+        *   \                          /
+        *    N                        N
+        *
+        */
+        if ((insertedNode == parent.rightChild) && (parent == grandparent.leftChild)) {
+            rotateLeft(parent, grandparent)
+            parent = insertedNode
+            insertedNode = insertedNode.leftChild ?: throw IllegalArgumentException("Node should have child after left rotation")
+        }
+        else if ((insertedNode == parent.leftChild) && (parent == grandparent.rightChild)) {
+            rotateRight(parent, grandparent)
+            parent = insertedNode
+            insertedNode = insertedNode.rightChild ?: throw IllegalArgumentException("Node should have right child after right rotation")
+        }
+
+        /* case 5: grandparent, parent and node form a "line"
+        *
+        *      G                   G
+        *     /                     \
+        *    P    - left line        P   - right line
+        *   /                         \
+        *  N                           N
+        *
+        */
+        parent.color = Color.BLACK
+        grandparent.color = Color.RED
+        if ((insertedNode == parent.leftChild)&&(parent == grandparent.leftChild)) {
+            rotateRight(grandparent, findParent(grandparent))
+        }
+        else {
+            rotateLeft(grandparent, findParent(grandparent))
+        }
+        return
     }
     private fun balanceAfterDelete(curNode: RBNode<K, V>) {
         TODO("Not yet implemented")
