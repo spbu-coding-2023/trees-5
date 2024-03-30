d5cae7fa212f7437450cc5c9ce325ffdc88fab7f
Shvorob <soniashv23@gmail.com>
2024-03-30 18:54:30 +0300

    fix(RBT): Implement class of red-black nodes

--- lib/src/main/kotlin/nodes/RBNode.kt
+++ lib/src/main/kotlin/nodes/RBNode.kt
@@ -1,4 +1,9 @@
 package nodes
 
-class RBNode {
+enum class Color {
+    RED, BLACK
+}
+
+class RBNode<K : Comparable<K>, V>(key: K, value: V): abstractNode<K, V, RBNode<K, V>>(key, value) {
+    var color: Color = Color.RED
 }
